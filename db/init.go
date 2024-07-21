ee8832b3661122af4bd5fc1257a71ea01f9753d7
daichi0525 <dai.0525n@gmail.com>
2024-07-21 18:08:17 +0900

    DB作成時のUserIdを数字のみに変更

--- db/init.go
+++ db/init.go
@@ -43,16 +43,16 @@ func Init() {
 func initializeData(db *gorm.DB) {
 	// スケジュールのテストデータを作成
 	schedules := []model.Schedule{
-		{UserId: "user1", Title: "Meeting with Bob", Priority: "High", StartTime: time.Now().Add(48 * time.Hour), EndTime: time.Now().Add(50 * time.Hour), Detail: "Discuss project details", Completed: false, CreatedAt: time.Now(), UpdatedAt: time.Now()},
-		{UserId: "user2", Title: "Project deadline", Priority: "Medium", StartTime: time.Now().Add(72 * time.Hour), EndTime: time.Now().Add(74 * time.Hour), Detail: "Submit final project report", Completed: true, CreatedAt: time.Now(), UpdatedAt: time.Now()},
-		{UserId: "user3", Title: "Gym workout", Priority: "Low", StartTime: time.Now().Add(24 * time.Hour), EndTime: time.Now().Add(25 * time.Hour), Detail: "Cardio session", Completed: false, CreatedAt: time.Now(), UpdatedAt: time.Now()},
-		{UserId: "user4", Title: "Doctor appointment", Priority: "High", StartTime: time.Now().Add(120 * time.Hour), EndTime: time.Now().Add(122 * time.Hour), Detail: "Routine check-up", Completed: true, CreatedAt: time.Now(), UpdatedAt: time.Now()},
-		{UserId: "user5", Title: "Team meeting", Priority: "Medium", StartTime: time.Now().Add(96 * time.Hour), EndTime: time.Now().Add(98 * time.Hour), Detail: "Project planning", Completed: false, CreatedAt: time.Now(), UpdatedAt: time.Now()},
-		{UserId: "user6", Title: "Call with client", Priority: "High", StartTime: time.Now().Add(36 * time.Hour), EndTime: time.Now().Add(37 * time.Hour), Detail: "Discuss contract terms", Completed: true, CreatedAt: time.Now(), UpdatedAt: time.Now()},
-		{UserId: "user7", Title: "Dentist appointment", Priority: "Low", StartTime: time.Now().Add(60 * time.Hour), EndTime: time.Now().Add(61 * time.Hour), Detail: "Dental cleaning", Completed: false, CreatedAt: time.Now(), UpdatedAt: time.Now()},
-		{UserId: "user8", Title: "Conference", Priority: "High", StartTime: time.Now().Add(144 * time.Hour), EndTime: time.Now().Add(146 * time.Hour), Detail: "Tech conference", Completed: true, CreatedAt: time.Now(), UpdatedAt: time.Now()},
-		{UserId: "user9", Title: "Lunch with friend", Priority: "Low", StartTime: time.Now().Add(168 * time.Hour), EndTime: time.Now().Add(169 * time.Hour), Detail: "Catch up over lunch", Completed: false, CreatedAt: time.Now(), UpdatedAt: time.Now()},
-		{UserId: "user10", Title: "Finish report", Priority: "Medium", StartTime: time.Now().Add(200 * time.Hour), EndTime: time.Now().Add(202 * time.Hour), Detail: "Finalize quarterly report", Completed: true, CreatedAt: time.Now(), UpdatedAt: time.Now()},
+		{UserId: "1", Title: "Meeting with Bob", Priority: "High", StartTime: time.Now().Add(48 * time.Hour), EndTime: time.Now().Add(50 * time.Hour), Detail: "Discuss project details", Completed: false, CreatedAt: time.Now(), UpdatedAt: time.Now()},
+		{UserId: "2", Title: "Project deadline", Priority: "Medium", StartTime: time.Now().Add(72 * time.Hour), EndTime: time.Now().Add(74 * time.Hour), Detail: "Submit final project report", Completed: true, CreatedAt: time.Now(), UpdatedAt: time.Now()},
+		{UserId: "1", Title: "Gym workout", Priority: "Low", StartTime: time.Now().Add(24 * time.Hour), EndTime: time.Now().Add(25 * time.Hour), Detail: "Cardio session", Completed: false, CreatedAt: time.Now(), UpdatedAt: time.Now()},
+		{UserId: "2", Title: "Doctor appointment", Priority: "High", StartTime: time.Now().Add(120 * time.Hour), EndTime: time.Now().Add(122 * time.Hour), Detail: "Routine check-up", Completed: true, CreatedAt: time.Now(), UpdatedAt: time.Now()},
+		{UserId: "1", Title: "Team meeting", Priority: "Medium", StartTime: time.Now().Add(96 * time.Hour), EndTime: time.Now().Add(98 * time.Hour), Detail: "Project planning", Completed: false, CreatedAt: time.Now(), UpdatedAt: time.Now()},
+		{UserId: "1", Title: "Call with client", Priority: "High", StartTime: time.Now().Add(36 * time.Hour), EndTime: time.Now().Add(37 * time.Hour), Detail: "Discuss contract terms", Completed: true, CreatedAt: time.Now(), UpdatedAt: time.Now()},
+		{UserId: "1", Title: "Dentist appointment", Priority: "Low", StartTime: time.Now().Add(60 * time.Hour), EndTime: time.Now().Add(61 * time.Hour), Detail: "Dental cleaning", Completed: false, CreatedAt: time.Now(), UpdatedAt: time.Now()},
+		{UserId: "2", Title: "Conference", Priority: "High", StartTime: time.Now().Add(144 * time.Hour), EndTime: time.Now().Add(146 * time.Hour), Detail: "Tech conference", Completed: true, CreatedAt: time.Now(), UpdatedAt: time.Now()},
+		{UserId: "2", Title: "Lunch with friend", Priority: "Low", StartTime: time.Now().Add(168 * time.Hour), EndTime: time.Now().Add(169 * time.Hour), Detail: "Catch up over lunch", Completed: false, CreatedAt: time.Now(), UpdatedAt: time.Now()},
+		{UserId: "2", Title: "Finish report", Priority: "Medium", StartTime: time.Now().Add(200 * time.Hour), EndTime: time.Now().Add(202 * time.Hour), Detail: "Finalize quarterly report", Completed: true, CreatedAt: time.Now(), UpdatedAt: time.Now()},
 	}
 
 	// スケジュールのテストデータをデータベースに保存
