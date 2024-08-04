1bdef7d0800828058fe5d3e12397797237f2f434
daichi0525 <dai.0525n@gmail.com>
2024-08-04 19:30:37 +0900

    リクエストに合わせて編集機能を実装

--- handler/todo_handler.go
+++ handler/todo_handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 	"time"
@@ -43,23 +44,63 @@ func (th *ScheduleHandler) GetSchedules(c *gin.Context) {
 // UpdateSchedule メソッドの追加
 func (th *ScheduleHandler) UpdateSchedule(c *gin.Context) {
 	var json struct {
-		Title     string    `json:"title"`
-		StartTime time.Time `json:"start_time"`
-		EndTime   time.Time `json:"end_time"`
+		ID         uint   `json:"id"`
+		Week       string `json:"week"`
+		Title      string `json:"title"`
+		StartTimeH string `json:"start_time_hour"`
+		StartTimeM string `json:"start_time_minute"`
+		EndTimeH   string `json:"end_time_hour"`
+		EndTimeM   string `json:"end_time_minute"`
 	}
 	if err := c.ShouldBindJSON(&json); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "JSONエラーです。"})
 		return
 	}
+	fmt.Println(json)
 
-	scheduleID := c.Param("id") // URL パラメータからスケジュールIDを取得
-	id, err := strconv.ParseUint(scheduleID, 10, 32)
+	// scheduleID := c.Param("id") // URL パラメータからスケジュールIDを取得
+	// id, err := strconv.ParseUint(scheduleID, 10, 32)
+	// if err != nil {
+	// 	c.JSON(http.StatusBadRequest, gin.H{"error": "無効なID"})
+	// 	return
+	// }
+
+	// 日付文字列をパース
+	date, err := time.Parse("2006-01-02", json.Week)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "無効なID"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid date format"})
 		return
 	}
 
-	err = th.scheduleUsecase.UpdateSchedule(uint(id), json.Title, json.StartTime, json.EndTime)
+	StartTimeH, err := strconv.Atoi(json.StartTimeH)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid date format"})
+		return
+	}
+
+	StartTimeM, err := strconv.Atoi(json.StartTimeM)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid date format"})
+		return
+	}
+
+	EndTimeH, err := strconv.Atoi(json.EndTimeH)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid date format"})
+		return
+	}
+
+	EndTimeM, err := strconv.Atoi(json.EndTimeM)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid date format"})
+		return
+	}
+
+	// time.Time型に変換
+	StartTime := time.Date(date.Year(), date.Month(), date.Day(), StartTimeH, StartTimeM, 0, 0, time.UTC)
+	EndTime := time.Date(date.Year(), date.Month(), date.Day(), EndTimeH, EndTimeM, 0, 0, time.UTC)
+
+	err = th.scheduleUsecase.UpdateSchedule(uint(json.ID), json.Title, StartTime, EndTime)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "更新に失敗しました"})
 		return
